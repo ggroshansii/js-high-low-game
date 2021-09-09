8f330b9782c717482bdfeda4c3328b50cb2c26f0
ggroshansii <garthgroshansii@gmail.com>
2021-09-09 16:49:43 -0400

    working on getting Player.cardCount to automatically update based on deck.length; not currently functioning

--- main.js
+++ main.js
@@ -2,65 +2,6 @@
 // function StartGame () {
 // }
 
-// function Player({name, deck, isTurn, cardCount} = {}) {
-//     this.name = name,
-//     this.deck = deck,
-//     this.isTurn = isTurn,
-//     this.cardCount = cardCount
-// }
-
-// function Deck({deck = []} = {}) {
-//     this.deck = deck,
-//     this.suites = ['clubs', 'diamonds', 'hearts', 'spades'];
-//     this.buildDeck = function() {
-//         if (this.deck.length === 0) {
-//         for (let suite of this.suites) {
-//             for (let i = 2; i <= 14; i++) {
-//                 if (i === 11) {
-//                     this.deck.push(`Jack of ${suite}`);
-//                 } else if (i === 12) {
-//                     this.deck.push(`Queen of ${suite}`);
-//                 } else if (i === 13) {
-//                     this.deck.push(`King of ${suite}`);
-//                 } else if (i === 14) {
-//                     this.deck.push(`Ace of ${suite}`);
-//                 } else {
-//                     this.deck.push(`${i} of ${suite}`);
-//                 }
-//             }
-//         }
-//     }
-//     return this.deck;
-// }
-
-// }
-
-
-// function Card({deck}) {
-//     let deckSize = 52;
-//     //this.deck 
-//     //random function on that particular deck
-//     //pulls out random card
-// }
-
-
-// const player1 = new Player({name: "Garth"});
-// const player2 = new Player({name: "Carlsen"});
-
-// let mainDeck = new Deck();
-// console.log(mainDeck.buildDeck());
-
-// function shuffleAndDeal() {
-
-// }
-
-// // player1.deck = 
-
-// // const Player2Deck = new Deck()
-
-
-// console.log(Math.floor(Math.random() * 52));
-
 function Card(value, suite) {
     this.value = value,
     this.suite = suite
@@ -70,11 +11,11 @@ function Player({ name, deck = [], isTurn = false, cardCount = null } = {}) {
     this.name = name,
     this.deck = deck,
     this.isTurn = isTurn,
-    this.cardCount = cardCount
+    this.cardCount = deck.length
 
     Player.prototype.playCard = function () {
         let card;
-        console.log(this.cardCount)
+        console.log(this.deck.length)
         let index = Math.floor(Math.random() * cardCount)
         if (this.deck.length > 0 && isTurn === true) {
             card = this.deck[index];
@@ -82,8 +23,7 @@ function Player({ name, deck = [], isTurn = false, cardCount = null } = {}) {
         } else {
 
         }
-        this.cardCount--;
-        console.log(this.cardCount);
+        console.log(this.deck.length);
         return this.deck[index];
     }
 }
@@ -164,9 +104,19 @@ for (let i = 0; i < player1.deck.length; i++) {
     }
 }
 
-console.log(player1.playCard())
-console.log(player2)
 
 function playWar() {
+    player1Card = player1.playCard();
+    player2Card = player2.playCard();
+    console.log(player1Card, player2Card);
+    console.log("p1 cc", player1.cardCount);
+    console.log("p2 cc", player2.cardCount);
 
+    if (player1Card.value > player2Card.value) {
+        player1.deck.push(player2Card);
+        console.log("p1 cc", player1.cardCount);
+        console.log("p2 cc", player2.cardCount);
+    }
 }
+
+playWar()

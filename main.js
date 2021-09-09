615f2ffd90e46c4b14b538fbe8000c50f9c4598e
ggroshansii <garthgroshansii@gmail.com>
2021-09-09 16:22:41 -0400

    added .playCard() method on the Player constructor

--- main.js
+++ main.js
@@ -63,19 +63,34 @@
 
 function Card(value, suite) {
     this.value = value,
-        this.suite = suite
+    this.suite = suite
 }
 
 function Player({ name, deck = [], isTurn = false, cardCount = null } = {}) {
     this.name = name,
-        this.deck = deck,
-        this.isTurn = isTurn,
-        this.cardCount = cardCount
+    this.deck = deck,
+    this.isTurn = isTurn,
+    this.cardCount = cardCount
+
+    Player.prototype.playCard = function () {
+        let card;
+        console.log(this.cardCount)
+        let index = Math.floor(Math.random() * cardCount)
+        if (this.deck.length > 0 && isTurn === true) {
+            card = this.deck[index];
+            this.deck.splice(index, 1);
+        } else {
+
+        }
+        this.cardCount--;
+        console.log(this.cardCount);
+        return this.deck[index];
+    }
 }
 
 function Deck() {
     this.deck = [],
-        this.suites = ['clubs', 'diamonds', 'hearts', 'spades'];
+    this.suites = ['clubs', 'diamonds', 'hearts', 'spades'];
     this.buildDeck = function () {
         if (this.deck.length === 0) {
             for (let suite of this.suites) {
@@ -115,20 +130,18 @@ function Deck() {
                 player1.deck.push(this.deck[index])
                 this.deck.splice(index, 1);
             }
+            player1.cardCount = player1.deck.length;
+            player2.cardCount = player2.deck.length;
             cardCount--;
         }
     }
 }
 
-
-const player1 = new Player({ name: "Garth" });
-const player2 = new Player({ name: "Carlsen" });
+const player1 = new Player({ name: "Garth", isTurn: true});
+const player2 = new Player({ name: "Carlsen", isTurn: false });
 let mainDeck = new Deck();
 console.log(mainDeck.buildDeck());
 console.log(mainDeck.shuffleAndDeal(player1, player2));
-
-
-
 
 
 let count = 0;
@@ -141,8 +154,6 @@ for (let i = 0; i < player1.deck.length; i++) {
     }
 }
 
-
-
 count = 0
 for (let i = 0; i < player1.deck.length; i++) {
 
@@ -153,5 +164,9 @@ for (let i = 0; i < player1.deck.length; i++) {
     }
 }
 
-console.log(player1.deck)
-console.log(player2.deck)
+console.log(player1.playCard())
+console.log(player2)
+
+function playWar() {
+
+}

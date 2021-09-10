19896fc78344b40e76a56c4b373ae5093b7e0afa
ggroshansii <garthgroshansii@gmail.com>
2021-09-10 12:58:51 -0400

    in the process of changing playCard() method from using random function to using array methods (push, shift, etc) instead so game will simulate real life better

--- main.js
+++ main.js
@@ -2,37 +2,44 @@
 // function StartGame () {
 // }
 
+let playerOneCardsInPlay = [];
+let playerTwoCardsInPlay = [];
+let gameOn = true;
+
 function Card(value, suite) {
     this.value = value,
-    this.suite = suite
+        this.suite = suite
 }
 
 function Player({ name, deck = [], isTurn = false, cardCount = null } = {}) {
     this.name = name;
     this.deck = deck;
     this.isTurn = isTurn;
-    this.cardCount = this.deck.length;
+
+    Object.defineProperty(this, 'cardCount', {
+        get() {
+            return this.deck.length;
+        }
+    })
 
     Player.prototype.playCard = function () {
         let card;
-        console.log(deck.length)
-        let index = Math.floor(Math.random() * cardCount)
-        if (this.deck.length > 0 && isTurn === true) {
-            console.log(this.deck.length)
-            card = this.deck[index];
-            this.deck.splice(index, 1); // Don't think it should get spliced here; probably get taken out of deck and added into other in playWar() function
-
+        let currentCardIndex;
+        currentCardIndex = Math.floor(Math.random() * this.cardCount)
+        console.log("CARD COUNT", this.cardCount, this.name);
+        if (this.deck.length > 0) {
+            card = this.deck[currentCardIndex];
+            this.deck.splice(currentCardIndex, 1);
         } else {
-
+            gameOver();
         }
-        console.log(deck.length);
-        return this.deck[index];
+        return card;
     }
 }
 
 function Deck() {
     this.deck = [],
-    this.suites = ['clubs', 'diamonds', 'hearts', 'spades'];
+        this.suites = ['clubs', 'diamonds', 'hearts', 'spades'];
     this.buildDeck = function () {
         if (this.deck.length === 0) {
             for (let suite of this.suites) {
@@ -59,70 +66,58 @@ function Deck() {
         return this.deck;
     }
     Deck.prototype.shuffleAndDeal = function (player1, player2) {
-        let cardCount = 52;
-        while (this.deck.length > 0) {
-            let index = Math.floor(Math.random() * cardCount)
-            if (player1.deck.length > player2.deck.length) {
+        let mainDeckCount = 52;
+        while (mainDeckCount > 0) {
+            let index = Math.floor(Math.random() * mainDeckCount)
+            if (player1.cardCount > player2.cardCount) {
                 player2.deck.push(this.deck[index])
-                this.deck.splice(index, 1);
-            } else if (player1.deck.length < player2.deck.length) {
-                player1.deck.push(this.deck[index])
                 this.deck.splice(index, 1);
             } else {
                 player1.deck.push(this.deck[index])
                 this.deck.splice(index, 1);
             }
-            player1.cardCount = player1.deck.length;
-            player2.cardCount = player2.deck.length;
-            cardCount--;
+            mainDeckCount--;
         }
     }
 }
 
-const player1 = new Player({ name: "Garth", isTurn: true});
-const player2 = new Player({ name: "Carlsen", isTurn: false });
-let mainDeck = new Deck();
-console.log(mainDeck.buildDeck());
-console.log(mainDeck.shuffleAndDeal(player1, player2));
-
-
-let count = 0;
-for (let i = 0; i < player1.deck.length; i++) {
-
-    if (player1.deck[i] === undefined) {
-        continue;
-    } else {
-        count++;
-    }
-}
-
-count = 0
-for (let i = 0; i < player1.deck.length; i++) {
-
-    if (player1.deck[i] === undefined) {
-        continue;
-    } else {
-        count++;
-    }
-}
-
-
 function playWar() {
-    console.log(player1.deck.length);
     player1Card = player1.playCard();
     player2Card = player2.playCard();
-    
-    console.log(player1Card, player2Card);
-    console.log("p1 cc", player1.cardCount);
-    console.log("p2 cc", player2.cardCount);
 
+    console.log("player1" , player1Card);
+    console.log("player2", player2Card);
     if (player1Card.value > player2Card.value) {
+        console.log("Player 1 wins");
         player1.deck.push(player2Card);
-        console.log("p1 cc", player1.cardCount);
-        console.log("p2 cc", player2.cardCount);
-        console.log(player1.deck.length)
-        console.log(player2.deck)
+
+    } else if (player2Card.value > player1Card.value) {
+        console.log("Player 2 wins");
+        player2.deck.push(player1Card);
+
+    } else {
+        console.log('tie');
     }
 }
 
-playWar()
+
+
+
+
+function gameOver() {
+    console.log("Game Over");
+    gameOn = true;
+}
+//while loop with playWar() inside until gameOver = false = true;
+
+
+
+const player1 = new Player({ name: "Garth", isTurn: true });
+const player2 = new Player({ name: "Carlsen", isTurn: false });
+let mainDeck = new Deck();
+mainDeck.buildDeck();
+mainDeck.shuffleAndDeal(player1, player2);
+
+while (gameOn) {
+    playWar();
+}

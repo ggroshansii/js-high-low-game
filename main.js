72c31e64b10e326e707e22039fafe42a60616f85
ggroshansii <garthgroshansii@gmail.com>
2021-09-11 23:03:42 -0400

    added a function called toggleColor() that will change the color of the text based on suite

--- main.js
+++ main.js
@@ -1,107 +1,108 @@
-
 const drawBtn = document.querySelector(".btn");
 const playerCardText = document.querySelector(".player-card-text");
-const computerCardText = document.querySelector(".computer-card-text")
+const computerCardText = document.querySelector(".computer-card-text");
 
 let gameOn = true;
 let tieRound = false;
 let tieRoundPotOfCards = [];
 
 function Card(value, suite) {
-    this.value = value,
-        this.suite = suite
+    (this.value = value), (this.suite = suite);
 }
 
 function Player({ name, deck = [] } = {}) {
     this.name = name;
     this.deck = deck;
 
-    Object.defineProperty(this, 'cardCount', {
+    Object.defineProperty(this, "cardCount", {
         get() {
             return this.deck.length;
-        }
-    })
+        },
+    });
 
     Player.prototype.playCard = function () {
         let card;
         if (this.cardCount > 0) {
             card = this.deck.shift();
             return card;
-        } 
-    }
+        }
+    };
 
     Player.prototype.tieRoundAnte = function () {
         for (let i = 0; i < 3; i++) {
             let currentAnteCard = this.deck.shift();
             tieRoundPotOfCards.push(currentAnteCard);
         }
-    }
+    };
 }
 
 function Deck() {
-    this.deck = [],
-    this.suites = ['clubs &clubs;', 'diamonds &diams;', 'hearts &hearts;', 'spades &spades;'];
+    (this.deck = []),
+        (this.suites = [
+            "clubs &clubs;",
+            "diamonds &diams;",
+            "hearts &hearts;",
+            "spades &spades;",
+        ]);
 }
 
-    Deck.prototype.buildDeck = function () {
-        if (this.deck.length === 0) {
-            for (let suite of this.suites) {
-                for (let i = 2; i <= 14; i++) {
-                    if (i === 11) {
-                        let card = new Card(i, suite);
-                        this.deck.push(card);
-                    } else if (i === 12) {
-                        let card = new Card(i, suite);
-                        this.deck.push(card);
-                    } else if (i === 13) {
-                        let card = new Card(i, suite);
-                        this.deck.push(card);
-                    } else if (i === 14) {
-                        let card = new Card(i, suite);
-                        this.deck.push(card);
-                    } else {
-                        let card = new Card(i, suite);
-                        this.deck.push(card);
-                    }
+Deck.prototype.buildDeck = function () {
+    if (this.deck.length === 0) {
+        for (let suite of this.suites) {
+            for (let i = 2; i <= 14; i++) {
+                if (i === 11) {
+                    let card = new Card(i, suite);
+                    this.deck.push(card);
+                } else if (i === 12) {
+                    let card = new Card(i, suite);
+                    this.deck.push(card);
+                } else if (i === 13) {
+                    let card = new Card(i, suite);
+                    this.deck.push(card);
+                } else if (i === 14) {
+                    let card = new Card(i, suite);
+                    this.deck.push(card);
+                } else {
+                    let card = new Card(i, suite);
+                    this.deck.push(card);
                 }
             }
         }
-        return this.deck;
     }
-    
-
+    return this.deck;
+};
 
 function Game({ players, mainDeck } = {}) {
-    this.players = [new Player({ name: "Garth"}), new Player({ name: "Carlsen"})];
+    this.players = [
+        new Player({ name: "Garth" }),
+        new Player({ name: "Carlsen" }),
+    ];
     this.mainDeck = new Deck();
 }
 
 Game.prototype.shuffleAndDeal = function () {
     let mainDeckCount = 52;
     while (mainDeckCount > 0) {
-        let index = Math.floor(Math.random() * mainDeckCount)
+        let index = Math.floor(Math.random() * mainDeckCount);
         if (this.players[0].cardCount > this.players[1].cardCount) {
-            this.players[1].deck.push(this.mainDeck.deck[index])
+            this.players[1].deck.push(this.mainDeck.deck[index]);
             this.mainDeck.deck.splice(index, 1);
         } else {
-            this.players[0].deck.push(this.mainDeck.deck[index])
+            this.players[0].deck.push(this.mainDeck.deck[index]);
             this.mainDeck.deck.splice(index, 1);
         }
         mainDeckCount--;
     }
-}
+};
 
-Game.prototype.playWar = function() {
+Game.prototype.playWar = function () {
     if (!this.players[0].cardCount && !this.players[1].cardCount) {
         this.mainDeck.buildDeck();
         this.shuffleAndDeal(this.players[0], this.players[1]);
     }
     player1Card = this.players[0].playCard();
     player2Card = this.players[1].playCard();
-    console.log(player1Card);
-    console.log(player2Card);
-    console.log("player1", player1Card, this.players[0].cardCount);
-    console.log("player2", player2Card, this.players[1].cardCount);
+
     if (!this.players[0].cardCount || !this.players[1].cardCount) {
         return this.gameOver();
     }
@@ -110,30 +111,27 @@ Game.prototype.playWar = function() {
         console.log("Player 1 wins");
         this.players[0].deck.push(player1Card);
         this.players[0].deck.push(player2Card);
-        playerCardText.innerHTML = `${player1Card.value} of ${player1Card.suite}`;
-        computerCardText.innerHTML = `${player2Card.value} of ${player2Card.suite}`;
-
-
+        toggleColor(player1Card, player2Card);
     } else if (player2Card.value > player1Card.value) {
         console.log("Player 2 wins");
         this.players[1].deck.push(player2Card);
         this.players[1].deck.push(player1Card);
-        playerCardText.innerHTML= `${player1Card.value} of ${player1Card.suite}`;
-        computerCardText.innerHTML = `${player2Card.value} of ${player2Card.suite}`;
-
-    } else {
+        toggleColor(player1Card, player2Card);
+     } else {
         tieRound = true;
-        console.log('tie');
+        console.log("tie");
         tieRoundPotOfCards.push(player1Card);
         tieRoundPotOfCards.push(player2Card);
-        this.tie();
+    
+        toggleColor(player1Card, player2Card);
 
+        this.tie();
     }
-}
+};
 
 //while loop with playWar() inside until gameOver = false = true;
 
-Game.prototype.tie = function() {
+Game.prototype.tie = function () {
     while (tieRound) {
         this.players[0].tieRoundAnte();
         this.players[1].tieRoundAnte();
@@ -147,40 +145,37 @@ Game.prototype.tie = function() {
 
         console.log(tieRoundPotOfCards);
         if (tieBreakerP1.value > tieBreakerP2.value) {
-            console.log("p1-tie-card", tieBreakerP1, "p2-tie-card", tieBreakerP2);
-            console.log('player 1 has won the tiebreaker')
-            tieRoundPotOfCards.forEach(element => {
+            tieRoundPotOfCards.forEach((element) => {
                 this.players[0].deck.push(element);
-            })
-            this.players[0].deck.push(tieBreakerP1, tieBreakerP2); // adding the tieBreaker card too 
+            });
+            this.players[0].deck.push(tieBreakerP1, tieBreakerP2); // adding the tieBreaker card too
             tieRound = false;
             tieRoundPotOfCards = [];
-        }
-        else if (tieBreakerP1.value < tieBreakerP2.value) {
-            console.log("p1-tie-card", tieBreakerP1, "p2-tie-card", tieBreakerP2);
-            console.log('player 2 has won the tiebreaker')
-            tieRoundPotOfCards.forEach(element => {
+
+        toggleColor(tieBreakerP1, tieBreakerP2);
+
+        } else if (tieBreakerP1.value < tieBreakerP2.value) {
+            tieRoundPotOfCards.forEach((element) => {
                 this.players[1].deck.push(element);
-            })
+            });
             this.players[1].deck.push(tieBreakerP1, tieBreakerP2);
             tieRound = false;
             tieRoundPotOfCards = [];
-        }
-        else {
-            console.log("p1-tie-card", tieBreakerP1, "p2-tie-card", tieBreakerP2);
-            console.log('ANOTHER TIE')
+            toggleColor(tieBreakerP1, tieBreakerP2);
+
+        } else {
             tieRoundPotOfCards.push(tieBreakerP1);
             tieRoundPotOfCards.push(tieBreakerP2);
+
+            toggleColor(tieBreakerP1, tieBreakerP2);
         }
     }
-}
+};
 
-Game.prototype.gameOver = function() {
+Game.prototype.gameOver = function () {
     console.log("Game Over");
     gameOn = false;
-
-}
-
+};
 
 // const player1 = new Player({ name: "Garth"});
 // const player2 = new Player({ name: "Carlsen"});
@@ -188,18 +183,37 @@ Game.prototype.gameOver = function() {
 // mainDeck.buildDeck();
 // mainDeck.shuffleAndDeal(player1, player2);
 
-    
-
-const game = new Game()
-
-
+const game = new Game();
 
 // while (gameOn) {
 //     console.log("while Loop", gameOn)
 //     game.playWar();
 // }
 
-
-drawBtn.addEventListener('click', event => {
+drawBtn.addEventListener("click", (event) => {
     game.playWar();
-})
+});
+
+function toggleColor(player1, player2) {
+    console.log('fired');
+    if (
+        player1Card.suite.includes("hearts") ||
+        player1Card.suite.includes("diamonds")
+    ) {
+        playerCardText.classList.add("red");
+    } else {
+        playerCardText.classList.remove("red");
+
+    }
+    if (
+        player2Card.suite.includes("hearts") ||
+        player2Card.suite.includes("diamonds")
+    ) {
+        computerCardText.classList.add("red");
+    } else {
+        computerCardText.classList.remove("red");
+
+    }
+    playerCardText.innerHTML = `${player1.value} of ${player1.suite}`;
+    computerCardText.innerHTML = `${player2.value} of ${player2.suite}`;
+}
